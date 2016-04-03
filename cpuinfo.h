6e4dd4be31bd5775c72baaba63f51fa8050632a8
Torbjörn Rathsman <milasudril@gmail.com>
2016-04-03 17:13:53 +0200

    Documented CPUInfo and DataSource

--- cpuinfo.h
+++ cpuinfo.h
@@ -5,13 +5,27 @@ target[name[cpuinfo.h] type[include]]
 #ifndef GLINDA_CPUINFO_H
 #define GLINDA_CPUINFO_H
 
+/** \file cpuinfo.h \brief Routines for getting properties of the CPU
+ */
+
 #include <cstdint>
 
 namespace Glinda
 	{
+	/**\brief Routines for getting properties of the CPU
+	*/
 	namespace CPUInfo
 		{
 		static constexpr uint16_t word_test=0x00ff;
+
+		/**\brief Returns the endianness of the CPU.
+		 *
+		 * This function tests the endianness of the CPU
+		 *
+		 * \return A non-zero value when the CPU is big endian, and zero
+		 * otherwise.
+		 *
+		*/
 		inline bool constexpr bigEndianIs()
 			{
 			return ( *(reinterpret_cast<const uint8_t*>(&word_test))==0x0);
--- datasource.h
+++ datasource.h
@@ -2,6 +2,9 @@
 target[name[datasource.h] type[include]]
 #endif
 
+/**\file datasource.h \brief Definition of an abstract data source
+ */
+
 #ifndef GLINDA_DATASOURCE_H
 #define GLINDA_DATASOURCE_H
 
@@ -9,13 +12,39 @@ target[name[datasource.h] type[include]]
 
 namespace Glinda
 	{
+	/**\brief An abstract data source.
+	 *
+	 * A DataSource is an object that can be read and that has a name.
+	 */
 	class DataSource
 		{
 		public:
+			/**\brief Reads data from ths source.
+			 *
+			 * This method shall read at most `length` bytes from the data
+			 * source into `buffer`. The method shall return the number of bytes
+			 * actually read. If this value is less than `length`, the caller
+			 * may assume an end of file was reached.
+			 *
+			*/
 			virtual size_t read(void* buffer, size_t length)=0;
+
+			/**\brief Retrievs the name of the source.
+			 *
+			 * This method shall return a name identifying the data source.
+			 *
+			*/
 			virtual const char* nameGet() const noexcept=0;
+
 			virtual ~DataSource()=default;
 
+			/**\brief Function pointer type for a DataSource destruction
+			 * function
+			 *
+			 * A Deleter shall destroy the object pointed to by `data_source`.
+			 *
+			 * \warning The function must not throw any exceptions.
+			*/
 			typedef void (*Deleter)(DataSource* data_source);
 		};
 	}
