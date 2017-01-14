8b4cde7f6afe5ed8653debbb10659c55d1d02f47
Torbjörn Rathsman <milasudril@gmail.com>
2017-01-14 20:45:17 +0100

    ...

--- program.hpp
+++ program.hpp
@@ -10,30 +10,9 @@ namespace Angle
 	class Program
 		{
 		public:
-			template<class ExceptionHandler,class ... T>
-			Program(ExceptionHandler&& eh,const Shader& shader,const T&...shaders)
-				{
-				assert(glCreateProgram!=nullptr);
-				m_handle=glCreateProgram();
-				GLuint handles[sizeof...(T)+1];
-				collect(handles,shader,shaders...);
-				for(size_t k=0;k<sizeof...(T)+1;++k)
-					{glAttachShader(m_handle,handles[k]);}
-				glLinkProgram(m_handle);
-				auto status=static_cast<GLint>(0);
-				glGetProgramiv(m_handle,GL_LINK_STATUS,&status);
-				if(status!=GL_TRUE)
-					{
-					auto length=static_cast<GLint>(0);
-					glGetProgramiv(m_handle,GL_INFO_LOG_LENGTH,&length);
-					char message[1024];
-					glGetShaderInfoLog(m_handle,std::min(1024,length),NULL,message);
-					for(size_t k=0;k<sizeof...(T)+1;++k)
-						{glDetachShader(m_handle,handles[k]);}
-					glDeleteProgram(m_handle);
-					r_eh->raise(Error("It was not possible to link the shader program. ",message));
-					}
-				}
+			template<class ... T>
+			Program(const Shader& shader,const T&...shaders);
+
 
 			~Program()
 				{glDeleteProgram(m_handle);}
@@ -49,6 +28,31 @@ namespace Angle
 				}
 			GLuint m_handle;
 		};
+
+	template<class ... T>
+	Program::Program(const Shader& shader,const T&...shaders)
+		{
+		assert(glCreateProgram!=nullptr);
+		m_handle=glCreateProgram();
+		GLuint handles[sizeof...(T)+1];
+		collect(handles,shader,shaders...);
+		for(size_t k=0;k<sizeof...(T)+1;++k)
+			{glAttachShader(m_handle,handles[k]);}
+		glLinkProgram(m_handle);
+		auto status=static_cast<GLint>(0);
+		glGetProgramiv(m_handle,GL_LINK_STATUS,&status);
+		if(status!=GL_TRUE)
+			{
+			auto length=static_cast<GLint>(0);
+			glGetProgramiv(m_handle,GL_INFO_LOG_LENGTH,&length);
+			char message[1024];
+			glGetShaderInfoLog(m_handle,std::min(1024,length),NULL,message);
+			for(size_t k=0;k<sizeof...(T)+1;++k)
+				{glDetachShader(m_handle,handles[k]);}
+			glDeleteProgram(m_handle);
+			r_eh->raise(Error("It was not possible to link the shader program. ",message));
+			}
+		}
 	}
 
 #endif
