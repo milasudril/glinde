7286145e52ede722c248248bb9e54b789c347fb2
Torbjörn Rathsman <milasudril@gmail.com>
2017-01-19 15:02:07 +0100

    Move member check foundation to separate file

--- window.hpp
+++ window.hpp
@@ -3,23 +3,14 @@
 #ifndef GLFWMM_WINDOW_HPP
 #define GLFWMM_WINDOW_HPP
 
+#include "membercheck.hpp"
+
 #include <GLFW/glfw3.h>
 #include <type_traits>
 
 
-#ifndef GLFWMM_NO_MEMBER_ADVERTISMENT
-#define ADVERTISE_MEMBER(membername) [[deprecated("Hint: add " #membername " to current class.")]]
-#else
-#define ADVERTISE_MEMBER(membername) 
-#endif
-
 namespace GLFWmm
 	{
-	struct has_not_member{};
-	struct has_member:public has_not_member{};
-	template<class X> struct member_test
-		{typedef int type;};
-
 	class WindowBase
 		{
 		public:
