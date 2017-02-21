a11cc5cd6577de1224a58d8ea8a881f80d2a8cd4
Torbjörn Rathsman <milasudril@gmail.com>
2017-02-21 09:44:30 +0100

    Initial work on ImageRenderer

--- shader.hpp
+++ shader.hpp
@@ -1,4 +1,7 @@
-//@	{"targets":[{"name":"shader.hpp","type":"include"}]}
+//@	{
+//@	"targets":[{"name":"shader.hpp","type":"include"}]
+//@	,"dependencies_extra":[{"ref":"shader.o","rel":"implementation"}]
+//@	}
 
 #ifndef ANGLE_SHADER_HPP
 #define ANGLE_SHADER_HPP
@@ -76,46 +79,24 @@ namespace Angle
 		private:
 			GLuint m_handle;
 		};
-
-	Shader::Shader(const char* source,ShaderType type)
-		{
-		assert(glCreateShader!=nullptr);
-		m_handle=glCreateShader(native_type(type));
-		assert(m_handle!=0);
-
-		auto length=static_cast<GLint>(strlen(source));
-		glShaderSource(m_handle,1,&source,&length);
-		glCompileShader(m_handle);
-		auto status=static_cast<GLint>(0);
-		glGetShaderiv(m_handle,GL_COMPILE_STATUS,&status);
-		if(status!=GL_TRUE)
-			{
-			auto length=static_cast<GLint>(0);
-			glGetShaderiv(m_handle,GL_INFO_LOG_LENGTH,&length);
-			char message[1024];
-			glGetShaderInfoLog(m_handle,std::min(1024,length),NULL,message);
-			glDeleteShader(m_handle);
-			exceptionRaise(Error("It was not possible to compile the ",name(type),". ",message));
-			}
-		}
 	}
 
-Angle::Shader operator "" _comp(const char* source,size_t length)
+inline Angle::Shader operator "" _comp(const char* source,size_t length)
 	{return Angle::Shader(source,Angle::ShaderType::COMPUTE_SHADER);}
 
-Angle::Shader operator "" _vert(const char* source,size_t length)
+inline Angle::Shader operator "" _vert(const char* source,size_t length)
 	{return Angle::Shader(source,Angle::ShaderType::VERTEX_SHADER);}
 
-Angle::Shader operator "" _tesc(const char* source,size_t length)
+inline Angle::Shader operator "" _tesc(const char* source,size_t length)
 	{return Angle::Shader(source,Angle::ShaderType::TESS_CONTROL_SHADER);}
 
-Angle::Shader operator "" _tese(const char* source,size_t length)
+inline Angle::Shader operator "" _tese(const char* source,size_t length)
 	{return Angle::Shader(source,Angle::ShaderType::TESS_EVALUATION_SHADER);}
 
-Angle::Shader operator "" _geom(const char* source,size_t length)
+inline Angle::Shader operator "" _geom(const char* source,size_t length)
 	{return Angle::Shader(source,Angle::ShaderType::GEOMETRY_SHADER);}
 
-Angle::Shader operator "" _frag(const char* source,size_t length)
+inline Angle::Shader operator "" _frag(const char* source,size_t length)
 	{return Angle::Shader(source,Angle::ShaderType::FRAGMENT_SHADER);}
 
 #endif
