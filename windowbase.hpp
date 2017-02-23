628684859b29e9cabbed81f9d59d5fcb9bd28ae8
Torbjörn Rathsman <milasudril@gmail.com>
2017-02-23 11:14:41 +0100

    Added cursor position query

--- windowbase.hpp
+++ windowbase.hpp
@@ -5,6 +5,7 @@
 
 #include "session.hpp"
 #include "size.hpp"
+#include <utility>
 
 namespace GLFWmm
 	{
@@ -52,20 +53,26 @@ namespace GLFWmm
 
 			typedef GLFWwindow* ContextHandle;
 
-			Size sizeGet() const noexcept
+			Size size() const noexcept
 				{
 				Size ret;
 				glfwGetWindowSize(m_handle,&ret.width,&ret.height);
 				return ret;
 				}
 
-			Size sizeFbGet() const noexcept
+			Size sizeFb() const noexcept
 				{
 				Size ret;
 				glfwGetFramebufferSize(m_handle,&ret.width,&ret.height);
 				return ret;
 				}
 
+			std::pair<double,double> cursorPos() const noexcept
+				{
+				std::pair<double,double> ret;
+				glfwGetCursorPos(m_handle,&ret.first,&ret.second);
+				return ret;
+				}
 
 			bool shouldClose() const noexcept
 				{return glfwWindowShouldClose(m_handle);}
@@ -100,8 +107,6 @@ namespace GLFWmm
 				glfwSetInputMode(m_handle,GLFW_STICKY_KEYS,static_cast<int>(mode));
 				return *this;
 				}
-
-			
 
 
 		protected:
