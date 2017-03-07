e033a996327332117d34f7e2a95200bf7bc9c94c
Torbjörn Rathsman <milasudril@gmail.com>
2017-03-07 10:09:25 +0100

    Lower message seq

--- messagequeue.cpp
+++ messagequeue.cpp
@@ -2,6 +2,7 @@
 
 #include "messagequeue.hpp"
 #include "message.hpp"
+#include <algorithm>
 
 using namespace Glinde;
 
@@ -14,5 +15,6 @@ bool MessageQueue::get(Message& message) noexcept
 	auto& msg=const_cast<Message&>( m_queue.top() );
 	message=std::move(msg);
 	m_queue.pop();
+	m_seq_next=std::min(message.seq(),m_seq_next);
 	return 1;
 	}
