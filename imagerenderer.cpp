b90dcc76daf4c0d69a36bf401d98f5537036579c
Torbjörn Rathsman <milasudril@gmail.com>
2017-03-08 16:54:11 +0100

    Do not inline CTOR

--- imagerenderer.cpp
+++ imagerenderer.cpp
@@ -8,3 +8,31 @@ using namespace Glinde;
 constexpr Angle::VertexAttribute ImageRenderer::ShaderLayout::attributes[];
 constexpr GeoSIMD::Point<float> ImageRenderer::s_points[];
 
+ImageRenderer::ImageRenderer():m_vertices(6)
+	,m_program(R"EOF(#version 450 core
+layout(location=0) in vec4 position;
+layout(location=1) uniform vec2 mirror;
+out vec2 tex_coords;
+void main()
+	{
+	gl_Position=position;
+	tex_coords=position.xy*mirror;
+	tex_coords=0.5*(tex_coords + vec2(1,1));
+	}
+)EOF"_vert,R"EOF(#version 450 core
+out vec4 color;
+in vec2 tex_coords;
+layout(location=0) uniform sampler2D texture_data;
+
+void main()
+	{
+	color=texture(texture_data,tex_coords);
+	}
+)EOF"_frag)
+	{
+	m_vertices.bufferData(native_type(s_points),6);
+	m_vao.vertexBuffer<0>(m_vertices).enableVertexAttrib<0>();
+	m_program.bind();
+	glUniform2f(1,1.0f,1.0f);
+	m_program.unbind();
+	}
--- imagerenderer.hpp
+++ imagerenderer.hpp
@@ -17,34 +17,7 @@ namespace Glinde
 	class ImageRenderer
 		{
 		public:
-			ImageRenderer():m_vertices(6)
-				,m_program(R"EOF(#version 450 core
-layout(location=0) in vec4 position;
-layout(location=1) uniform vec2 mirror;
-out vec2 tex_coords;
-void main()
-	{
-	gl_Position=position;
-	tex_coords=position.xy*mirror;
-	tex_coords=0.5*(tex_coords + vec2(1,1));
-	}
-)EOF"_vert,R"EOF(#version 450 core
-out vec4 color;
-in vec2 tex_coords;
-layout(location=0) uniform sampler2D texture_data;
-
-void main()
-	{
-	color=texture(texture_data,tex_coords);
-	}
-)EOF"_frag)
-				{
-				m_vertices.bufferData(native_type(s_points),6);
-				m_vao.vertexBuffer<0>(m_vertices).enableVertexAttrib<0>();
-				m_program.bind();
-				glUniform2f(1,1.0f,1.0f);
-				m_program.unbind();
-				}
+			ImageRenderer();
 
 			void render(const Angle::Texture2D& texture) const noexcept
 				{
