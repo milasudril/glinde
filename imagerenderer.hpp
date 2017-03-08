fb396188bf9b402e5a5afa7ce810f5b0baddfa17
Torbjörn Rathsman <milasudril@gmail.com>
2017-03-08 16:38:13 +0100

    Flip image

--- imagerenderer.hpp
+++ imagerenderer.hpp
@@ -20,11 +20,12 @@ namespace Glinde
 			ImageRenderer():m_vertices(6)
 				,m_program(R"EOF(#version 450 core
 layout(location=0) in vec4 position;
+layout(location=1) uniform vec2 mirror;
 out vec2 tex_coords;
 void main()
 	{
 	gl_Position=position;
-	tex_coords=position.xy*vec2(1,-1);
+	tex_coords=position.xy*mirror;
 	tex_coords=0.5*(tex_coords + vec2(1,1));
 	}
 )EOF"_vert,R"EOF(#version 450 core
@@ -40,16 +41,9 @@ void main()
 				{
 				m_vertices.bufferData(native_type(s_points),6);
 				m_vao.vertexBuffer<0>(m_vertices).enableVertexAttrib<0>();
-				}
-
-			void render(const Angle::Texture2D& texture,Angle::Framebuffer& fb) const noexcept
-				{
-			//	TODO: bind fb
-				m_vao.bind();
-				texture.bind(0);
 				m_program.bind();
-				glUniform1i(0,0);
-				glDrawArrays(GL_TRIANGLES,0,6);
+				glUniform2f(1,1.0f,1.0f);
+				m_program.unbind();
 				}
 
 			void render(const Angle::Texture2D& texture) const noexcept
@@ -57,10 +51,18 @@ void main()
 				m_vao.bind();
 				texture.bind(0);
 				m_program.bind();
-				glUniform1i(0,0);
 				glDrawArrays(GL_TRIANGLES,0,6);
 				}
 
+			ImageRenderer& mirror(bool x,bool y) noexcept
+				{
+				m_program.bind();
+				glUniform2f(1
+					,x?-1.0f:1.0f
+					,y?-1.0f:1.0f);
+				m_program.unbind();
+				return *this;
+				}
 			
 		private:
 			struct ShaderLayout
